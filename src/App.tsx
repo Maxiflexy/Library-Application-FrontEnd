fc4250449ed75fc03646dd2d365160f3a6bbf697
Maxiflexy <onyemax247@gmail.com>
2024-07-07 23:05:43 +0100

    created carousel consuming return book component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
+import { Carousel } from "./layouts/Homepage/Carousel";
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+    </div>
   );
 }
 
---
+++ src/layouts/Homepage/Carousel.tsx
@@ -0,0 +1,80 @@
+import { ReturnBook } from "./ReturnBook";
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const Carousel = () => {
+  return (
+    <div className="container mt-5" style={{ height: 550 }}>
+      <div className="homepage-carousel-title">
+        <h3>Find your next "I stayed up too late reading" books.</h3>
+      </div>
+      <div
+        id="carouselExampleControls"
+        className="carousel carousel-dark slide mt-5 d-none d-lg-block"
+        data-bs-interval="false"
+      >
+        {/* Desktop */}
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="row d-flex justify-content-center align-items-center">
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+            </div>
+          </div>
+
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+            </div>
+          </div>
+
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      {/* Mobile */}
+      <div className="d-lg-none mt-3">
+        <ReturnBook />
+      </div>
+
+      <div className="homepage-carousel-title mt-3">
+        <a className="btn btn-outline-secondary btn-lg" href="#">
+          View More
+        </a>
+      </div>
+    </div>
+  );
+};
