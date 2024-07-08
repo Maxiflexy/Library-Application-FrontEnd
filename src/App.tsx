99f82c66a86728d0fcd618777df4b69723932314
Maxiflexy <onyemax247@gmail.com>
2024-07-08 22:18:03 +0100

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/Homepage/HomePage";
 import { SeachBooksPage } from "./layouts/SearchBooksPage/SeachBooksPage";
+import { Route } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SeachBooksPage />
+      <Route path="/">
+        <HomePage />
+      </Route>
+
+      <Route path="/search">
+        <SeachBooksPage />
+      </Route>
+
       <Footer />
     </div>
   );
-}
+};
