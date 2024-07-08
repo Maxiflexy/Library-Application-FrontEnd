a596726fb1569621d5ca3efee2210a3f65d2cee1
Maxiflexy <onyemax247@gmail.com>
2024-07-08 16:47:57 +0100

    created SearchBook component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/Homepage/HomePage";
+import { SeachBooksPage } from "./layouts/SearchBooksPage/SeachBooksPage";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SeachBooksPage />
       <Footer />
     </div>
   );
--- src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -4,11 +4,11 @@ import BookModel from "../../../models/BookModel";
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="card mt-3 shadow p-3 mb-3 bg-body rounded">
-      <div className="col-md-2">
-        <div className="row g-0">
+      <div className="row g-0">
+        <div className="col-md-2">
           <div className="d-none d-lg-block">
             {props.book.img ? (
-              <img src="{props.book.img}" width="123" height="196" alt="Book" />
+              <img src={props.book.img} width="123" height="196" alt="Book" />
             ) : (
               <img
                 src={require("../../../Images/BooksImages/book-luv2code-1000.png")}
