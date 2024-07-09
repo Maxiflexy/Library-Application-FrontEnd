006fd965739e5e980d9b82dc0f65e29c4f888012
Maxiflexy <onyemax247@gmail.com>
2024-07-09 13:17:22 +0100

    updated StartsReviews component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -76,7 +76,7 @@ export const BookCheckoutPage = () => {
               <h2>{book?.title}</h2>
               <h5 className="text-primary">{book?.author}</h5>
               <p className="lead">{book?.description}</p>
-              <StarsReviews rating={4} size={32} />
+              <StarsReviews rating={4.5} size={32} />
             </div>
           </div>
         </div>
@@ -100,6 +100,7 @@ export const BookCheckoutPage = () => {
             <h2>{book?.title}</h2>
             <h5 className="text-primary">{book?.author}</h5>
             <p className="lead">{book?.description}</p>
+            <StarsReviews rating={4.5} size={32} />
           </div>
         </div>
         <hr />
--- src/layouts/utils/StarsReviews.tsx
+++ src/layouts/utils/StarsReviews.tsx
@@ -3,9 +3,37 @@ import React from "react";
 export const StarsReviews: React.FC<{ rating: number; size: number }> = (
   props
 ) => {
+    
+    let rating = props.rating;
+
+    let fullStars = 0;
+
+    let halfStars = 0;
+
+    let emptyStars = 0;
+
+    if(rating !== undefined && rating > 0 && rating <= 5){
+        for(let i = 0; i <= 4; i++){
+            if(rating -1 >= 0){
+                fullStars = fullStars + 1;
+                rating = rating - 1;
+            }else if(rating === .5){
+                halfStars = halfStars + 1;
+                rating = rating - .5;
+            }else if(rating == 0){
+                emptyStars = emptyStars + 1;
+            }else{
+                break;
+            }
+        }
+    }else{
+        emptyStars = 5;
+    }
+
+
   return (
     <div>
-      {Array.from({ length: 2 }, (_, i) => (
+      {Array.from({ length: fullStars }, (_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +48,7 @@ export const StarsReviews: React.FC<{ rating: number; size: number }> = (
         </svg>
       ))}
 
-      {Array.from({ length: 1 }, (_, i) => (
+      {Array.from({ length: halfStars }, (_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
@@ -35,7 +63,7 @@ export const StarsReviews: React.FC<{ rating: number; size: number }> = (
         </svg>
       ))}
 
-      {Array.from({ length: 2 }, (_, i) => (
+      {Array.from({ length: emptyStars }, (_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
