358a59a6d8a2de9d0a8724613d0d28dc90758be3
Maxiflexy <onyemax247@gmail.com>
2024-07-09 09:32:44 +0100

    updated bookcheckout page

--- src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
 import BookModel from "../../../models/BookModel";
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
@@ -39,9 +40,9 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
           </div>
         </div>
         <div className="col-md-4 d-flex justify-content-center align-items-center">
-          <a className="btn btn-md main-color text-white" href="#">
+          <Link className="btn btn-md main-color text-white" to={`/checkout/${props.book.id}`}>
             View Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
