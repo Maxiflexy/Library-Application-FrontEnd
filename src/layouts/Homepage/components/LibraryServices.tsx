ee5fe12a00939704d0fe853ba74d795909c154fc
Maxiflexy <onyemax247@gmail.com>
2024-07-10 23:00:24 +0100

    updated library services component

--- src/layouts/Homepage/components/LibraryServices.tsx
+++ src/layouts/Homepage/components/LibraryServices.tsx
@@ -1,6 +1,13 @@
+import { useOktaAuth } from "@okta/okta-react"
+import { Link } from "react-router-dom";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export const LibraryServices = () => {
+
+  const {authState} = useOktaAuth();
+
   return (
+
     <div className="container my-5">
       <div className="row p-4 align-items-center border shadow-lg">
         <div className="col-lg-7 p-3">
@@ -12,7 +19,14 @@ export const LibraryServices = () => {
             admin's a personal message!
           </p>
           <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
-            <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+            {authState?.isAuthenticated? 
+            <Link to="#" type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white">
+              Library Services
+            </Link>  
+            :
+
+          }
+            
           </div>
         </div>
         <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
