6d4e1da313efe664efea138249983af717d227df
Maxiflexy <onyemax247@gmail.com>
2024-07-16 23:06:10 +0100

    created AddNEwBook component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
+import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibraryPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -63,6 +64,9 @@ export const App = () => {
             </SecureRoute>
             <SecureRoute path="/messages">
               <MessagesPage />
+            </SecureRoute>
+            <SecureRoute path="/admin">
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
