f0e596e1d0f74a175e3c1b7f9aba75a95afeee6e
Maxiflexy <onyemax247@gmail.com>
2024-07-17 20:57:39 +0100

    updated nav-bar

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: "0oaiadh654KHMYTgm5d7", 
     issuer: "https://dev-61814843.okta.com/oauth2/default",
-    redirectUri: "http://localhost:3000/login/callback",
+    redirectUri: "https://localhost:3000/login/callback",
     scopes: ["openid", "profile", "email"],
     pkce: true,
     disableHttpsCheck: true,
