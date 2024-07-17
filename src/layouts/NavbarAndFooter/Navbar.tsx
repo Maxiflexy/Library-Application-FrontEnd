282164a395552d1bc5f94027dd9e2907134cb1a3
Maxiflexy <onyemax247@gmail.com>
2024-07-17 13:24:34 +0100

    updated nav-bar

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -44,6 +44,11 @@ export const Navbar = () => {
                 <NavLink className="nav-link" to="/shelf">Shelf</NavLink>
               </li>
             }
+            {authState.isAuthenticated && authState.accessToken?.claims?.sub === "rosemary@criticalthinkers.tech" &&
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/admin">Admin</NavLink>
+              </li>
+            }
           </ul>
 
           <ul className="navbar-nav ms-auto">
