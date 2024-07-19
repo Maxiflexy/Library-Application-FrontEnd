e91d065839bfe1a89868f25a488d0bd0d3c382a1
Maxiflexy <onyemax247@gmail.com>
2024-07-20 00:06:17 +0100

    strip dependecies

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App} from './App';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51Pdfdw2NsGxl8DWzSC0zlbEWjeIEMTUVjlA6wuIffMRBQlzby6hyJTpYpQrY8tA2PKJSr2zMMd16t0qANY0L54Vl008xsVl7HN"
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
